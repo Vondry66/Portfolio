7bef076808a599d09fda03bd9b50ba81c70f5910
vondry66 <vondry66@gmail.com>
2022-11-29 15:00:05 +0000

    adds logos and more about me

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import Header from './components/Header';
-import AboutMe from './components/AboutMe';
-import './App.css';
+import Header from "./components/Header";
+import AboutMe from "./components/AboutMe";
+import MoreAbout from "./components/MoreAboutMe";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <AboutMe />
+      <MoreAbout />
     </div>
   );
 }
---
+++ src/components/MoreAboutMe.jsx
@@ -0,0 +1,19 @@
+
+const MoreAbout =()=>{
+return (
+    <section class="more-about">
+    <h2>More About Me</h2>
+    <p>
+        I much prefer nonSQL database over the SQL ones, but I am more than happy to work with SQL ones too ! I slightly favor the front end side of programming. 
+        I have experience with kanbans like Trello. I am used to wireframes 
+    </p>
+    <p>
+      My hobbies include cars - mainly JDM. Video games, listening to metalcore, and to unwind, I enjoy creating DnD campaigns for my friends. 
+     
+    </p>
+   
+  </section>
+)
+}
+
+export default MoreAbout
